cd716494199cbca6e3102f38026f31c7b2c2e11c
belle <belletuen@gmail.com>
2019-01-26 22:51:15 -0500

    trying to make navigation work

--- app/src/main/java/com/example/recipeer/MainActivity.java
+++ app/src/main/java/com/example/recipeer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.recipeer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -83,7 +84,8 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.nav_home) {
             // Handle the camera action
         } else if (id == R.id.nav_my_recipes) {
-
+            Intent intent = new Intent(this, MyRecipies.class);
+            startActivity(intent);
         } else if (id == R.id.nav_saved_recipes) {
 
         } else if (id == R.id.nav_add_new_recipe) {
--- app/src/main/java/com/example/recipeer/MyRecipies.java
+++ app/src/main/java/com/example/recipeer/MyRecipies.java
@@ -1,5 +1,6 @@
 package com.example.recipeer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -19,6 +20,7 @@ public class MyRecipies extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_recipies);
+        Intent intent = getIntent();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
